d8244e6aeed728f4fb453126e844ae8c6330b79f
Rodrigo de Oliveira <rodrigo.oliveira@pearson.com>
2020-01-25 11:28:03 -0300

    Example App - Building IdentityServer4 ASP.NET Core 3.1 Server and a MVC Client as well

---
+++ MvcClient/Startup.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.IdentityModel.Protocols.OpenIdConnect;
+
+namespace MvcClient
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllersWithViews();
+
+            services.AddAuthentication(options =>
+            {
+                options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = OpenIdConnectDefaults.AuthenticationScheme;
+            })
+                .AddCookie(CookieAuthenticationDefaults.AuthenticationScheme)
+                .AddOpenIdConnect(OpenIdConnectDefaults.AuthenticationScheme, options =>
+                {
+                    options.Authority = "http://localhost:5000";
+                    options.RequireHttpsMetadata = false;
+                    options.ClientId = "mvc";
+                    options.SaveTokens = true;
+                    options.ClientSecret = "super-secret";
+                    options.ResponseType = OpenIdConnectResponseType.Code;
+                    options.Scope.Add("openid");
+                    options.Scope.Add("profile");
+                });
+            services.AddAuthorization();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapDefaultControllerRoute()
+                    .RequireAuthorization();
+            });
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,11 @@
+# ASP.NET Core 3.1 com IdentityServer4
+
+Example App - IdentityServer4 e ASP.NET Core 3.1 Server e um MVC Client
+
+Requisitos
+
+* ASP.NET Core 3.1
+
+# Para rodar
+
+2Configure ambos os projetos para rodarem juntos.
